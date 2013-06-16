3f1e1b8f5c8ccd8307b62d0bb680c410dd8ce781
wkp4666 <wkp4666@126.com>
2013-06-16 16:08:48 +0800

    get

--- config.c
+++ config.c
@@ -14,6 +14,7 @@ int getConfSectionValue(char *conf, char *section, char *keyName, char *keyVal)
 {
   char sectionname[32], keyname[32];
   FILE *fp;
+  char *buf,*c;
   char buf_i[KEYVALLEN], buf_o[KEYVALLEN];
   int found = 0;
   if((fp = fopen(conf, "r")) == NULL)
@@ -26,9 +27,57 @@ int getConfSectionValue(char *conf, char *section, char *keyName, char *keyVal)
   sprintf(sectionname, "[%s]", section);
   while( !feof(fp) && fgets( buf_i, KEYVALLEN, fp )!=NULL )
   {
+    l_trim(buf_o, buf_i);
+    if( strlen(buf_o) <= 0 )
+      continue;
+    buf = NULL;
+    buf = buf_o;  
+
+    if( found == 0 ){
+      if( buf[0] != '[' ) {
+        continue;
+      } else if ( strncmp(buf, sectionname, strlen(sectionname))==0 ){
+        found = 1;
+        continue;
+      }
+
+    } else if(found == 1 ){
+      if( buf[0] == ';' ){
+        continue;
+      } else if ( buf[0] == '[' ){
+        break;
+      } else{
+        if( (c = (char*)strchr(buf, '=')) == NULL )
+          continue;
+        memset( keyname, 0, sizeof(keyname) ); 
+
+	sscanf( buf, "%[^=|^ |^\t]", keyname );
+	if( strcmp(keyname, keyName) == 0 ){
+          sscanf( ++c, "%[^\n]", keyVal );
+	  char *KeyVal_o = (char *)malloc(strlen(keyVal) + 1);
+	  if(KeyVal_o != NULL){
+	    memset(KeyVal_o, 0, sizeof(KeyVal_o));
+	    a_trim(KeyVal_o, keyVal);
+	    if(KeyVal_o && strlen(KeyVal_o) > 0)
+	      strcpy(keyVal, KeyVal_o);
+	    free(KeyVal_o);
+	    KeyVal_o = NULL;
+          }
+          found = 2;
+          break;
+        }else{
+          continue;
+        }
+      }
+
+    }
 
   }
-
+  fclose( fp );
+  if( found == 2)
+    return 0;
+  else
+    return -1;
 }
 
 
@@ -46,12 +95,39 @@ char * l_trim(char * szOutput, const char *szInput)
  return strcpy(szOutput, szInput);
 }
 
+/*   删除右边的空格   */
+char *r_trim(char *szOutput, const char *szInput)
+{
+ char *p = NULL;
+ assert(szInput != NULL);
+ assert(szOutput != NULL);
+ assert(szOutput != szInput);
+ strcpy(szOutput, szInput);
+ for(p = szOutput + strlen(szOutput) - 1; p >= szOutput && isspace(*p); --p){
+  ;
+ }
+ *(++p) = '\0';
+ return szOutput;
+}
 
+/*   删除两边的空格   */
+char * a_trim(char * szOutput, const char * szInput)
+{
+ char *p = NULL;
+ assert(szInput != NULL);
+ assert(szOutput != NULL);
+ l_trim(szOutput, szInput);
+ for   (p = szOutput + strlen(szOutput) - 1;p >= szOutput && isspace(*p); --p){
+  ;
+ }
+ *(++p) = '\0';
+ return szOutput;
+}
 /* test */
 int main()
 {
  char ip[32];
- getConfSectionValue("./sample.ceph.conf", "osd.0", "hostname", ip);
+ getConfSectionValue("./sample.ceph.conf", "osd.0", "host", ip);
  printf("%s\n",ip);
  return 0;
 }
--- config.h
+++ config.h
@@ -6,3 +6,7 @@ int getConfSectionValue(char *conf, char *section, char *keyName, char *keyVal);
 
 int setConfSectionValue(char *conf, char *section, char *keyName, char *keyVal);
 
+
+char *l_trim(char * szOutput, const char *szInput);
+char *r_trim(char *szOutput, const char *szInput);
+char *a_trim(char * szOutput, const char * szInput);
