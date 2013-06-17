5aa31a5a2fb98312bf1955a427b1ebd50597460b
wkp4666 <wkp4666@126.com>
2013-06-17 22:44:12 +0800

    s

--- modify_config.h
+++ modify_config.h
@@ -1,9 +1,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-
+/*
+ * conf 为conf文件的路径 section 为 setcion  keyname 为key的名字
+ * keyVal 返回查到的值  lineNum返回所在行
+ * 返回0表示成功， -1 keyName不存在 -2 section不存在 -3为文件打开失败
+ */
 int getConfSectionValue(char *conf, char *section, char *keyName, char *keyVal, int *lineNum);
 
+/*
+ * type -1 keyName不存在 -2 section不存在
+ * 返回0 成功， -3 文件打开失败
+ */
 int setConfSectionValue(char *conf, char *section, char *keyName, char *keyVal, int lineNum, int type);
 
 
